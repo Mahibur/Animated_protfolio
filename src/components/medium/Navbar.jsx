c8f0abcc4d3c9fa6cee8b3219160147836d530d0
Mahibur <mmahibur10100@gmail.com>
2025-04-05 10:46:16 +0600

    navbar animation

--- src/components/medium/Navbar.jsx
+++ src/components/medium/Navbar.jsx
@@ -13,8 +13,9 @@ const Navbar = ()=>{
                         <img src={logo} alt="logo.png" className="w-13 md:w-17" />
                     </picture>
                 </div>
-                <div className="dropdown border border-transparent relative group py-4.5 px-[35px] bg-[rgba(2,2,2,0.4)] cursor-pointer z-[1] before:absolute  before:left-0 before:top-0 before:w-0 before:h-full before:bg-[#EBEBEB] before:z-[-1] flex justify-center items-center hover:before:w-full hover:border-white before:transition-all before:duration-750 transition-all duration-750">
-                    <HiOutlineMenuAlt4 className=" text-white group-hover:text-black transition-all duration-500"/>
+                <div className="dropdown border border-transparent relative group py-4.5 px-[35px] bg-[rgba(2,2,2,0.4)] cursor-pointer z-[1] before:absolute  before:left-0 before:top-0 before:w-0 before:h-full before:bg-[#EBEBEB] before:z-[-1] flex flex-col  gap-y-3 items-end hover:before:w-full hover:border-white before:transition-all before:duration-750 transition-all duration-750">
+                    <div className="w-17.5 h-[1.5px] bg-white group-hover:bg-black transition-all duration-500"></div>
+                    <div className="w-11 h-[1.5px] group-hover:w-17.5 bg-white group-hover:bg-black transition-all duration-500"></div>
                 </div>
             </Container>
         </nav>
