fb0bb47cb34e8c8508824257114cd7c41b9f2fdf
Mahibur <mmahibur10100@gmail.com>
2025-04-06 11:09:20 +0600

    dropdown

--- src/components/medium/DropDown.jsx
+++ src/components/medium/DropDown.jsx
@@ -1,9 +1,14 @@
 import React from "react"
-
+import logo from "../../assets/images/dark_logo.png"
 const DropDown =({className})=>{
     return(
-    <div className={`py-14 bg-red-500 ${className}`}>
-
+    <div className={`bg-blue-500 p-5 rounded-lg w-full ${className}`}>
+           <div className="logo">
+                            <picture>
+                                <img src={logo} alt="logo.png" className="w-5.5 md:w-10.5" />
+                            </picture>
+                        </div>
+        <h1>heading</h1>
         </div>
     )
 }
--- src/components/medium/Navbar.jsx
+++ src/components/medium/Navbar.jsx
@@ -5,7 +5,7 @@ import DropDown from "./DropDown";
 
 
 const Navbar = ()=>{
-    const [open,setOpen] =useState(false);
+    const [open,setOpen] =useState(true);
     return(
         <nav className="pt-10 fixed w-full top-0 left-0 z-100 px-1.5">
             <Container className="flex justify-between items-center relative">
@@ -14,13 +14,13 @@ const Navbar = ()=>{
                         <img src={logo} alt="logo.png" className="w-13 md:w-17" />
                     </picture>
                 </div>
-                <div onClick= {()=>setOpen(!open)} className="dropdown-button border border-transparent relative group py-4.5 px-6 md:px-[35px] bg-[rgba(2,2,2,0.4)] cursor-pointer z-[1] before:absolute  before:left-0 before:top-0 before:w-0 before:h-full before:bg-[#EBEBEB] before:z-[-1] flex flex-col  gap-y-2 md:gap-y-3 items-end hover:before:w-full hover:border-white before:transition-all before:duration-750 transition-all duration-750">
+                <div onClick= {()=>setOpen(!open)} className="dropdown-button border border-transparent rounded-sm relative group py-4.5 px-6 md:px-[35px] bg-[rgba(2,2,2,0.4)] cursor-pointer z-[1] before:absolute  before:left-0 before:top-0 before:w-0 before:h-full before:bg-[#EBEBEB] before:z-[-1] flex flex-col  gap-y-2 md:gap-y-3 items-end hover:before:w-full hover:border-white before:transition-all before:duration-750 transition-all duration-750">
                     <div className="w-12 md:w-17.5 h-[1.5px] bg-white group-hover:bg-black transition-all duration-500"></div>
                     <div className="w-6 md:w-11 h-[1.5px] group-hover:w-12 md:group-hover:w-17.5 bg-white group-hover:bg-black transition-all duration-500"></div>
                 </div>
                 {
                  open &&(
-                   <DropDown/>
+                   <DropDown className="absolute top-0 left-0"/>
                  )  
                 }
             </Container>
