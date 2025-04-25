66c9c80e08ae5e5cd3294c71d21b57da64a9d624
Mahibur <86772655+Mahibur@users.noreply.github.com>
2025-04-25 14:32:39 +0000

    changes device

--- src/components/medium/TextSection.jsx
+++ src/components/medium/TextSection.jsx
@@ -7,14 +7,15 @@ const TextSection = () => {
   return (
     <Container className="text pt-32 pb-22">
       <h1 className="font-inter text-6xl text-h1 text-center leading-[72px] relative">
-        create &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;innovative designs for modern
+        create <div className="img inline-block">
+          <div className="w-20.5 h-12.5 bg-h1 rounded-[100px] flex items-center pl-2.5">
+            <img className="animate-[spin_3s_linear_infinite]" src={icon} alt="" />
+          </div>
+        </div> innovative designs for modern
         websites, mobile apps,
         <span className="text-[#515151]">
           and web platforms across a wide range of industries.
         </span>
-        <div className="absolute top-[6%] left-[22%] w-20.5 h-12.5 bg-h1 rounded-[100px] flex items-center pl-2.5">
-          <img className="animate-[spin_3s_linear_infinite]" src={icon} alt="" />
-        </div>
       </h1>
     </Container>
   );
