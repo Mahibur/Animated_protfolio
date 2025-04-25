7e1cab1ee7212386db2436ae70bf0d9c744014e5
Mahibur <86772655+Mahibur@users.noreply.github.com>
2025-04-25 14:49:17 +0000

    changes

--- src/components/medium/Work.jsx
+++ src/components/medium/Work.jsx
@@ -7,7 +7,7 @@ import WorkContent from "../min/WorkContent"
         <div className="work mt-32 pt-44.5">
            <Container className="">
             <div className="text relative">
-            <h1 className="font-segoe text-[520px] tracking-[0.1px] text-h1 uppercase text-center max-w-full">Work</h1>
+            <h1 className="font-segoe text-[520px] tracking-[0.1px] text-h1 uppercase w-full">Work</h1>
             <img className="absolute top-1/2 left-1/2 translate-x-[-40%] translate-y-[-10%]" src={workLogo} alt="" />
             </div>
             <div className="content flex flex-col pt-67">
