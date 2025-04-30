951b309de49411d13813ed61c4011cba6a6ef50f
Mahibur <mmahibur10100@gmail.com>
2025-05-01 01:24:43 +0600

    animate text section

--- src/components/medium/AboutText.jsx
+++ src/components/medium/AboutText.jsx
@@ -1,10 +1,19 @@
 import React from "react"
+import Container from "../min/Container"
+import bg from "../../assets/images/animate_bg.png"
 const AboutText =()=>{
     return(
-        <div className="text py-80 flex justify-center items-center gap-y-7.5">
-            <p className="font-segoe text-base text-[#6A6A6A] tracking-[0.8] uppercase">/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About Me</p>
-            <h1 className="max-w-[1214px] font-segoe">I build visually striking designs 
-            that prioritize seamless user experiences.</h1>
+        <div className="text">
+            <Container>
+            <div className="headings flex flex-col justify-center items-center gap-y-7.5 py-80">
+            <p className="font-segoe text-base text-[#6A6A6A] tracking-[0.8px] uppercase text-center">/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About Me</p>
+            <h1 className="max-w-[1214px] font-segoe text-[56px] tracking-[0.1px] leading-[67.5px] text-center text-h1">I build visually striking designs<br/> 
+            <span className="italic">that prioritize seamless user experiences.</span></h1>
+            <p className="max-w-[612px] text-center font-segoe text-lg tracking-[0.1px] leading-[21.6px] text-body">With 05 years of experience in the design industry I have been helping to bring brands to life through thoughtful design that resonates.</p>
+            </div>
+            <img src={bg} alt="animate_bg.png" />
+            </Container>
+
         </div>
     )
 }
