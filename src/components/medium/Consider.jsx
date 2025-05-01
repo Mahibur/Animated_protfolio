5ba3285937e72522c8ef20ab8542105f6676c270
Mahibur <86772655+Mahibur@users.noreply.github.com>
2025-05-01 23:45:04 +0000

    consider

---
+++ src/components/medium/Consider.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import content from "../../assets/images/consider_bg.png"
+import Container from "../min/Container"
+
+const Consider =()=>{
+    return(
+        <div className="consider mt-32">
+            <Container>
+                
+            </Container>
+        </div>
+    )
+}
